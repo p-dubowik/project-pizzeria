ab305b4e3c0514c332b6179e1659cc5729613e0c
Paweł Dubowik <p.dubowik27@gmail.com>
2025-12-07 17:15:37 +0100

    Prepare site for online hosting

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -111,7 +111,7 @@
       defaultDeliveryFee: 20,
     },
     db: {
-      url: '//localhost:3131',
+      url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
       products: 'products',
       orders: 'orders',
       bookings: 'bookings',
